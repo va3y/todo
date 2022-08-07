5b5809cef5242f55910ab67c90b1f8e798d69ce3
Валерий Иванов <va.ivanov@cian.ru>
2022-08-07 11:35:59 +0300

    try querying

--- api/hello-world.ts
+++ api/hello-world.ts
@@ -1,7 +1,17 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handleRequest = async (req: VercelRequest, res: VercelResponse) => {
-	res.send("hello world");
+	await prisma.todo.create({
+		data: {
+			title: "af",
+		},
+	});
+	const allRecords = await prisma.todo.findMany();
+
+	res.send(res.json(allRecords));
 };
 
 export default handleRequest;
